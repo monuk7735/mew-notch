25f00f4e94d71a3fc3184938b0b8b88763e388b1
Monu Kumar <monuk7735@gmail.com>
2025-06-05 22:13:23 +0530

    fix: notch: expanded: camera
    - view was not hiding hence camera session was not closing

--- MewNotch/View/Notch/Expanded/ExpandedNotchView.swift
+++ MewNotch/View/Notch/Expanded/ExpandedNotchView.swift
@@ -58,6 +58,7 @@ struct ExpandedNotchView: View {
                         MirrorView(
                             notchViewModel: notchViewModel
                         )
+                        .id(notchViewModel.isExpanded)
                     case .NowPlaying:
                         NowPlayingDetailView(
                             namespace: namespace,
